ec6a03ef7f6fd4ca265608e8fe9e34e7c97c019e
KIMBtech <info@kimb-technologies.eu>
2019-09-27 14:55:13 +0200

    Update dockerpublish.sh

--- dockerpublish.sh
+++ dockerpublish.sh
@@ -7,10 +7,8 @@ docker images
 
 PHP_VERSION=$(docker run -it --rm $IMAGE_NAME sh -c 'echo "$PHP_VERSION"')
 PHP_VERSION=$(echo $PHP_VERSION | grep -o '[0-9\.]*')
-NGINX_VERSION=$(docker run -it --rm $IMAGE_NAME sh -c "nginx -v")                                                                                                                                                                1 ↵
-NGINX_VERSION=$(echo $NGINX_VERSION | grep -o '[0-9\.]*')
 
-for TAG in "php-$PHP_VERSION" "nginx-$NGINX_VERSION" "php-$PHP_VERSION-nginx-$NGINX_VERSION" "latest"; do
+for TAG in "php-$PHP_VERSION" "latest"; do
 	echo "Tagging Image as $TAG and pushing";
 	docker tag $IMAGE_NAME "kimbtechnologies/$IMAGE_NAME:$TAG"
       docker push "kimbtechnologies/$IMAGE_NAME:$TAG"
